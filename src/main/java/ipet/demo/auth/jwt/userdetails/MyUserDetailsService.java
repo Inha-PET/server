175606b345a7246476717d654bfb1cf314108664
CJY <c0512jy@naver.com>
2023-09-22 22:43:49 +0900

    feat: 회원가입/로그인 구현. JWT 토큰 생성 기능
    Spring security 기능을 사용하기 위해 UserDetails, UserDetailsService 인터페이스 상속.
    Access Token과 Refresh Token 구현

--- src/main/java/ipet/demo/auth/jwt/userdetails/MyUserDetailsService.java
+++ src/main/java/ipet/demo/auth/jwt/userdetails/MyUserDetailsService.java
@@ -1,4 +1,23 @@
 package ipet.demo.auth.jwt.userdetails;
 
-public class MyUserDetailsService {
+import ipet.demo.domain.member.Member;
+import ipet.demo.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MyUserDetailsService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Member member = memberRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("해당 유저를 찾을 수 없습니다."));
+
+        return new MyUserDetails(member);
+    }
 }
