49bfdd6cf02bb4a5a104499f0e48d45f7224c137
CJY <c0512jy@naver.com>
2023-09-27 00:29:54 +0900

    feat: JWT 함수 추가
    access token에 넣어둔 회원의 ID를 찾아오는 함수

--- src/main/java/ipet/demo/auth/jwt/provider/JwtTokenProvider.java
+++ src/main/java/ipet/demo/auth/jwt/provider/JwtTokenProvider.java
@@ -1,20 +1,28 @@
 package ipet.demo.auth.jwt.provider;
 
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import ipet.demo.auth.jwt.dto.TokenInfo;
 import ipet.demo.auth.jwt.userdetails.MyUserDetails;
 import ipet.demo.domain.member.Member;
+import ipet.demo.exception.BusinessLogicException;
+import ipet.demo.exception.ExceptionCode;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Date;
+import java.util.List;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -51,16 +59,48 @@ public class JwtTokenProvider {
                 .build();
     }
 
-    private String generateRefreshToken(Authentication authentication, Date refreshTokenExpiresIn) {
-        String authorities = getAuthorities(authentication);
-        Member member = getMember(authentication);
-        return Jwts.builder()
-                .setSubject(authentication.getName())
-                .claim("id", member.getId())
-                .claim("auth", authorities)
-                .setExpiration(refreshTokenExpiresIn)
-                .signWith(key, SignatureAlgorithm.HS256)
-                .compact();
+    public Long getMemberId(String accessToken) {
+        Claims claims = parseClaims(accessToken);
+
+        if (claims.get("id") == null) {
+            throw new BusinessLogicException(ExceptionCode.MEMBER_NOT_FOUND);
+        }
+
+        return claims.get("id", Long.class);
+    }
+
+    public Authentication getAuthentication(String accessToken) {
+        Claims claims = parseClaims(accessToken);
+
+        if (claims.get("auth") == null) {
+            throw new BusinessLogicException(ExceptionCode.MEMBER_NOT_FOUND);
+        }
+
+        //클레임에서 권한 정보 가져오기
+        Collection<? extends GrantedAuthority> authorities = getAuthorityList(claims);
+
+        //UserDetails 객체를 만들어서 Authentication 리턴
+        UserDetails principal = new User(claims.getSubject(), "", authorities);
+        return new UsernamePasswordAuthenticationToken(principal, "", authorities);
+    }
+
+    /**
+     * 토큰 정보를 검증하는 메서드
+     */
+    public boolean validateToken(String accessToken) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(accessToken);
+            return true;
+        } catch (io.jsonwebtoken.security.SecurityException | MalformedJwtException e) {
+            log.info("Invalid JWT Token", e);
+        } catch (ExpiredJwtException e) {
+            log.info("Expired JWT Token", e);
+        } catch (UnsupportedJwtException e) {
+            log.info("Unsupported JWT Token", e);
+        } catch (IllegalArgumentException e) {
+            log.info("JWT claims string is empty.", e);
+        }
+        return false;
     }
 
     private String generateAccessToken(Authentication authentication, Date accessTokenExpiresIn) {
@@ -75,15 +115,42 @@ public class JwtTokenProvider {
                 .compact();
     }
 
-    //Authentication 객체에서 권한 가져오기
+    private String generateRefreshToken(Authentication authentication, Date refreshTokenExpiresIn) {
+        String authorities = getAuthorities(authentication);
+        Member member = getMember(authentication);
+        return Jwts.builder()
+                .setSubject(authentication.getName())
+                .claim("id", member.getId())
+                .claim("auth", authorities)
+                .setExpiration(refreshTokenExpiresIn)
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    /**
+     * Authentication 객체에서 권한 가져오기
+     * @Return: ex) ROLE_USER, ROLE_ADMIN
+     */
     private String getAuthorities(Authentication authentication) {
         return authentication.getAuthorities().stream()
                 .map(GrantedAuthority::getAuthority)
                 .collect(Collectors.joining(","));
     }
-
     private Member getMember(Authentication authentication) {
         MyUserDetails principal = (MyUserDetails) authentication.getPrincipal();
         return principal.getMember();
+    }
+    private Claims parseClaims(String accessToken) {
+        try {
+            return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(accessToken).getBody();
+        } catch (ExpiredJwtException e) {
+            return e.getClaims();
+        }
+    }
+
+    private List<SimpleGrantedAuthority> getAuthorityList(Claims claims) {
+        return Arrays.stream(claims.get("auth").toString().split(","))
+                .map(SimpleGrantedAuthority::new)
+                .toList();
     }
 }
